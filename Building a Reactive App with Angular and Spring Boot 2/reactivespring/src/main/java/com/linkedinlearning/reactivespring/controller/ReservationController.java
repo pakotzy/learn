c3e2ddb3460923a986f1fe4f75435908d74c3282
Pavlo Kotelnytskyi <Pavlo_Kotelnytskyi@epam.com>
2020-10-05 08:21:01 +0300

    Remove unused imports

--- Building a Reactive App with Angular and Spring Boot 2/reactivespring/src/main/java/com/linkedinlearning/reactivespring/controller/ReservationController.java
+++ Building a Reactive App with Angular and Spring Boot 2/reactivespring/src/main/java/com/linkedinlearning/reactivespring/controller/ReservationController.java
@@ -4,7 +4,6 @@ import com.linkedinlearning.reactivespring.model.Reservation;
 import com.linkedinlearning.reactivespring.service.ReservationService;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpStatusCodeException;
 
 import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
 
 @RestController
 @RequestMapping("${application.path}" + ReservationController.RESOURCE_NAME)
