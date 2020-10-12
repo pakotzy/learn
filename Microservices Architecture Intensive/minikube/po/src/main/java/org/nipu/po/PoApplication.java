28e2e274312cdc197cfa4d964cb24d5bdcdac75a
Pavlo Kotelnytskyi <Pavlo_Kotelnytskyi@epam.com>
2020-10-12 15:25:03 +0300

    Clone from humb1t/java_inda_cloud, restructure

---
+++ Microservices Architecture Intensive/minikube/po/src/main/java/org/nipu/po/PoApplication.java
@@ -0,0 +1,15 @@
+package org.nipu.po;
+
+import org.nipu.po.order.clients.ProductSpecificationRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients(clients = ProductSpecificationRepository.class)
+public class PoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoApplication.class, args);
+    }
+}
